3e51b3efa440fe2585522a1f8ce82b2ec03c48bc
“TeddyXu7586” <3145954761@qq.com>
2021-07-31 21:23:21 +0800

    start to learn java on how2j

---
+++ src/HelloWorld.java
@@ -0,0 +1,5 @@
+public class Hero{
+        public static void main(String[] args){
+                System.out.println("hello world");
+        }
+    }
---
+++ src/Hero.java
@@ -0,0 +1,7 @@
+public class Hero {
+    String name;
+    float hp;
+    float armor;
+    int moveSpeed;
+    
+}
